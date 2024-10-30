fedb85cf7e7bc7205f7385b5ae6ac37238d2eced
aryanchari06 <aryaney0704@gmail.com>
2024-10-30 16:30:12 +0530

    multer middleware

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import { fs } from "fs";
+
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File is uploaded on cloudinary", response, response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); //removes file from our server which was temporary
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
